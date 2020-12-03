96df49ab5a732b92aaec0eca7aa1ca40f0edaa25
Joey Sheehy <joey.sheehy@students.ittralee.ie>
2020-12-03 13:43:32 +0000

    Commit to Mini Project

---
+++ src/Library/Main.java
@@ -0,0 +1,126 @@
+package Library;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.GregorianCalendar;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
+public class Main extends JFrame implements ActionListener {
+         JMenu booksMenu;
+         JMenu usersMenu;
+         JLabel imgLabel;
+         JLabel welcomeLabel;
+         JPanel wlcPanel;
+         JPanel buttonC;
+         JButton booksButton;
+         JButton usersButton;
+
+
+         TitledBorder titledBorder;
+         ArrayList<Books> books = new ArrayList<>();
+         private Books book;
+
+         public Main() {
+
+             setTitle("Apex Library");
+             setSize(400,300);
+             setLocationRelativeTo(null);
+             setResizable(false);
+             setVisible(true);
+
+             setIconImage(new ImageIcon(getClass().getResource("logo.png")).getImage());
+
+             Container cPane = getContentPane();
+             setLayout(new FlowLayout());
+
+             JMenuBar menuBar = new JMenuBar();
+             setJMenuBar(menuBar);
+             menuBar.setBackground(Color.LIGHT_GRAY);
+
+             menuBar.add(booksMenu);
+             menuBar.add(usersMenu);
+
+
+             wlcPanel = new JPanel();
+             wlcPanel.add(Box.createVerticalStrut(10));
+             wlcPanel.setLayout(new BoxLayout(wlcPanel, BoxLayout.Y_AXIS));
+
+             welcomeLabel = new JLabel("Welcome to Apex Library");
+             welcomeLabel.setFont(new Font("serif",4,25));
+             welcomeLabel.setForeground(Color.RED);
+
+             welcomeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+             wlcPanel.add(welcomeLabel);
+             wlcPanel.add(Box.createVerticalStrut(30));
+
+             try {
+                 imgLabel = new JLabel();
+                 imgLabel.setIcon(new ImageIcon(getClass().getResource("logo.png")));
+
+                 imgLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+                 wlcPanel.add(imgLabel);
+             }
+             catch(Exception ex) {
+
+                 JOptionPane.showMessageDialog(null,"Invalid Image File in Main Screen");
+             }
+
+             wlcPanel.add(Box.createVerticalStrut(30));
+
+             buttonC = new JPanel();
+
+             titledBorder = new TitledBorder("Click one of the buttons");
+             titledBorder.setTitleColor(Color.RED);
+
+             buttonC.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
+             buttonC.setPreferredSize(new Dimension(390,50));
+             buttonC.setBorder(titledBorder);
+             buttonC.setLayout(new GridLayout(1,3));
+
+             booksButton = new JButton("View Books");
+             booksButton.addActionListener(this);
+             buttonC.add(booksButton);
+
+             usersButton = new JButton("View Users");
+             usersButton.addActionListener(this);
+             buttonC.add(usersButton);
+
+             add(wlcPanel);
+             add(buttonC);
+
+             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+             setVisible(true);
+
+
+         }
+
+         public void createBookMenu() {
+             JMenuItem item;
+
+             booksMenu = new JMenu("Books");
+             booksMenu.setMnemonic(KeyEvent.VK_C);
+
+             item = new JMenuItem("View Books");
+             item.setMnemonic(KeyEvent.VK_A);
+             item.addActionListener(this);
+             booksMenu.add(item);
+         }
+
+
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
+
